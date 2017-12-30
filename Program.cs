3bbe4fc0389d4bf15040b5c631257e77d4baca82
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-12-30 13:19:15 +0900

    Run 'dotnet new console'

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_trial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
