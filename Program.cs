5dd850bf53a930afdf42bfaa7cc49c63aaa17890
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-12-31 13:50:10 +0900

    Fix something

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Text.RegularExpressions;
 namespace dotnet_trial {
     class Program {
         static void Main (string[] args) {
-            var re = new Regex ("a((?<=^aa*)a)*");
+            var re = new Regex ("a(?:(?<=^aa*)a)*");
             for (int i = 0, p = 1; i <= 14; i++, p *= 2) {
                 var s = new string ('a', p);
                 var sw = new Stopwatch ();
@@ -14,7 +14,7 @@ namespace dotnet_trial {
                     re.Match (s);
                 }
                 sw.Stop ();
-                Console.WriteLine (String.Format ("{0} {1:F5}", p, sw.Elapsed.TotalSeconds));
+                Console.WriteLine (String.Format ("{0} {1:F5}", p, sw.Elapsed.TotalMilliseconds));
             }
         }
     }
