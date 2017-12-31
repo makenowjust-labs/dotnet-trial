b1353ded4514a218691f78af9b8bcc136c5e440e
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-12-31 13:41:03 +0900

    Update program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
-namespace dotnet_trial
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace dotnet_trial {
+    class Program {
+        static void Main (string[] args) {
+            var re = new Regex ("a((?<=^aa*)a)*");
+            for (int i = 0, p = 1; i <= 14; i++, p *= 2) {
+                var s = new string ('a', p);
+                var sw = new Stopwatch ();
+                sw.Start ();
+                for (int j = 0; j < 100; j++) {
+                    re.Match (s);
+                }
+                sw.Stop ();
+                Console.WriteLine (String.Format ("{0} {1:F5}", p, sw.Elapsed.TotalSeconds));
+            }
         }
     }
 }
